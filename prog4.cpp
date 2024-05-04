f839b30f39a75b124efc5dbbfd8acf5a72506896
Chris <chirschbrich@smu.edu>
2024-05-03 23:01:52 -0500

    finished + comments

--- prog4.cpp
+++ prog4.cpp
@@ -8,13 +8,18 @@ istream& operator>>(istream& is, Applicant& a);
 ostream& operator<<(ostream& os, Applicant& a);
 
 pair<int, vector<Applicant>> pickTeam(int budget, vector<vector<Applicant>>& alist) {
-    int P = alist.size();
-    vector<vector<float>> dp(P + 1, vector<float>(budget + 1, 0));
-    vector<vector<int>> chosen(P + 1, vector<int>(budget + 1, -1));
+    int positions = alist.size(); // Number of positions
+    vector<vector<float>> dp(positions + 1, vector<float>(budget + 1, 0)); // Store best benefit for max budget
+    vector<vector<int>> chosen(positions + 1, vector<int>(budget + 1, -1)); // Store chosen applicant for each position and budget
 
-    for (int i = 1; i <= P; i++) {
+    // Check if there are applicants
+    if (positions == 0 || alist.empty()) {
+        return make_pair(0, vector<Applicant>());
+    }
+
+    for (int i = 1; i <= positions; i++) {
         for (int j = 0; j <= budget; j++) {
-            dp[i][j] = dp[i-1][j];  // no applicant is chosen for the i-th position
+            dp[i][j] = dp[i-1][j];  // No applicant is chosen for the i-th position
             for (int l = 0; l < alist[i-1].size(); l++) {
                 if (j >= alist[i - 1][l].cost && dp[i - 1][j - alist[i - 1][l].cost] + alist[i - 1][l].benefit > dp[i][j]) {
                     dp[i][j] = dp[i - 1][j - alist[i - 1][l].cost] + alist[i - 1][l].benefit;
@@ -25,17 +30,58 @@ pair<int, vector<Applicant>> pickTeam(int budget, vector<vector<Applicant>>& ali
     }
 
     vector<Applicant> team;
-    for (int i = P, j = budget; i > 0; i--) {
+    for (int i = positions, j = budget; i > 0; i--) {
         if (chosen[i][j] != -1) {
             team.push_back(alist[i - 1][chosen[i][j]]);
             j -= alist[i - 1][chosen[i][j]].cost;
         }
     }
 
-    return make_pair(dp[P][budget], team);
+    return make_pair(dp[positions][budget], team);
 }
 
-pair<float, vector<Applicant>> pickTeam_2(int budget, vector<vector<Applicant>>& alist) {
-    vector<Applicant> app;
-    return make_pair(0, app);
+pair<int, vector<Applicant>> pickTeam_2(int budget, vector<vector<Applicant>>& alist) {
+    int positions = alist.size(); // Number of positions
+    vector<vector<float>> dp(positions + 1, vector<float>(budget + 1, 0)); // Store best benefit for max budget 
+    vector<vector<vector<Applicant>>> chosen(positions + 1, vector<vector<Applicant>>(budget + 1)); // Store chosen applicants for each position and budget
+
+    // Check if there are applicants
+    if (positions == 0 || alist.empty()) {
+        return make_pair(0, vector<Applicant>());
+    }
+
+    for (int i = 1; i <= positions; i++) {
+        for (int j = 0; j <= budget; j++) {
+            dp[i][j] = dp[i - 1][j];
+            chosen[i][j] = chosen[i - 1][j];
+
+            for (int m = 0; m < alist[i - 1].size(); m++) {
+                Applicant& a1 = alist[i - 1][m];
+                if (a1.cost <= j) { // Check if the cost of a1 is within the budget
+                    float benefit = dp[i - 1][j - a1.cost] + a1.benefit; // Calculate the benefit of selecting a1
+                    if (benefit > dp[i][j]) { // Check if the benefit is greater than the current best benefit
+                        dp[i][j] = benefit;
+                        chosen[i][j] = chosen[i - 1][j - a1.cost];
+                        chosen[i][j].push_back(a1);
+                    }
+                }
+
+                for (int k = m + 1; k < alist[i - 1].size(); k++) {
+                    Applicant& a2 = alist[i - 1][k];
+                    // Check if the combined cost of a1 and a2 is within the budget
+                    if (a1.cost + a2.cost <= j) { 
+                        float aBenefits = dp[i - 1][j - (a1.cost + a2.cost)] + a1.benefit + a2.benefit;
+                        if (aBenefits > dp[i][j]) { // Check if the total benefits are greater than the current best benefit
+                            dp[i][j] = aBenefits;
+                            chosen[i][j] = chosen[i - 1][j - (a1.cost + a2.cost)]; // Update the chosen applicants
+                            chosen[i][j].push_back(a1); 
+                            chosen[i][j].push_back(a2);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return make_pair(dp[positions][budget], chosen[positions][budget]);
 }
